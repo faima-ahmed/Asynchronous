9a2f61e9e009b45bb8a37486badd96a0770e66d3
faima-ahmed <faimaahmeddiya@gmail.com>
2024-10-09 12:13:14 +0600

    Commit 1

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+const paymentSuccess= true;
+const marks= 70;
+
+function enroll(callback){
+    console.log('Course enrollment is in progress.');
+
+    setTimeout(function(){
+        if(paymentSuccess){
+            callback();
+        }
+        else{
+            console.log('payment failed');
+        }
+    }, 2000);
+}
+
+function progress(callback){
+    console.log('Course on progress...');
+
+    setTimeout(function(){
+        if(marks >= 80){
+            callback();
+        }
+        else{
+            console.log('You could not get enough')
+        }
+    })
+}
